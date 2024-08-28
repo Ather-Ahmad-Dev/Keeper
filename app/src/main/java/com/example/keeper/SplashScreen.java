d245405ee155e05374797a69ff1802aa7b93db11
Ather-Ahmad-Dev <atherahmadwork@gmail.com>
2024-08-28 04:31:38 -0700

    Splash Screen Added

---
+++ app/src/main/java/com/example/keeper/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.example.keeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.keeper.databinding.ActivitySplashScreenBinding;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ActivitySplashScreenBinding binding;
+    private final int SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+
+    }
+}
